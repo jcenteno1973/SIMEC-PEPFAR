1c32321fd25df0f73807643173e38cb6be4ca7c8
nereira07 <yamilethcampos12@gmail.com>
2016-12-08 21:36:34 -0600

    Cambio de boton de ayuda

--- resources/views/usuario_app/ingresar.blade.php
+++ resources/views/usuario_app/ingresar.blade.php
@@ -58,41 +58,7 @@
           
           <td>
               <!-- <button type="button" class="btn btn-primary">Ayuda</button> -->
-              
-             <a href="#ingreso" class="btn btn- btn-primary" data-toggle="modal">Ayuda</a>
-               <div class="modal fade" id="ingreso" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content"> 
-                            <!--header de la ventana-->
-                            <div class="modal-header">                    
-                                <p class="modal-title"> Ayuda para ingresar al sistema</p>
-                            </div>
-                            <!-- Contenido de la ventana -->
-                            <div class="modal-body">
-                                 <table> 
-                                    <tr>
-                                        <td>
-                                            <img src="{{asset('images/informativo.png')}}" alt="informativo" class="img-thumbnail"/>                                
-                                        </td>
-                                        <td>
-                                             <p ><font color="white">...</font></p>
-                                        </td>
-                                        <td>
-                                             <p>contenido de la ventana</p>
-                                        </td>
-                                    </tr>
-
-                                </table>  
-
-                            </div>
-                            <!--footer de la ventana-->
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
+			  @include('usuario_app/ayuda_usuario/ayuda_ingreso')			
           </td>
         </tr>
         </table>
