25117e96608a85eb3eda3ccfed922dbe63bf183e
nereira07 <yamilethcampos12@gmail.com>
2016-12-10 06:43:25 -0600

    agregando ayuda

---
+++ resources/views/usuario_app/ayuda_usuario/ayuda_editar_rol.blade.php
@@ -0,0 +1,56 @@
+<!--Boton de ayuda-->
+    <a href="#buscarusuario" class="btn btn- btn-primary" data-toggle="modal">Ayuda</a>
+        <div class="modal fade" id="buscarusuario" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog"> <!---div class="modal-dialog">-->
+                <div class="modal-content"> 
+                    <!--header de la ventana-->
+                    <div class="modal-header">                    
+                        <p class="modal-title"> EDITAR ROL</p>
+                    </div>
+                    <!-- Contenido de la ventana -->
+                    <div class="modal-body">
+                    <table> 
+                        <tr>
+                            <td>
+                                <img src="{{asset('images/informativo.png')}}" alt="informativo"/>
+                            </td>
+                            <td>
+                                <p> Para realizar cambios en un rol realice los siguiente:</p>
+                            </td>
+                        <tr>    
+                            <td>
+                                <p><font color="white">...</font></p>
+                            </td>
+                            <td>
+                                <ul>
+                                    <li><strong>Rol</strong>: Elija de la lista desplegable el rol a modificar.</li>
+                                    <li><strong>Nombre del Rol</strong>: podr&aacute; modificar el nombre del rol.</li>
+                                    <li><strong>Descripci&oacute;n</strong> podr&aacute; modificar la descipcion del rol.</li>
+                                    <li><strong>Botones</strong>:
+                                        <ul>
+                                            <li>Guardar: Guarda y actualiza los cambios solicitados</li>
+                                            <li>Regresar: lo llevar&aacute; a la ventana anterior</li>
+                                            <li>Ayuda: Muestra una descripci&oacute;n de los elementos de la pantalla</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p ><font color="white">...</font></p>
+                            </td>
+                        </tr>                    
+                    </table>  
+
+                    </div>
+                    <!--footer de la ventana-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+<!--FIN Boton de ayuda-->
+        
