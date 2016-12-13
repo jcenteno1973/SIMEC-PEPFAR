bf29a982b0abaf26ec2377a8800b6845cf08f18e
nereira07 <yamilethcampos12@gmail.com>
2016-12-13 16:28:16 -0600

    Agregando ruta a plantilla y  modificando pantalla Ppal.

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -20,41 +20,18 @@
 @stop 
 @section('contenido')
 <div class="panel panel-default">
-    <div class="panel-heading" align="center">MENU PRINCIPAL</div>
-    <div class="panel-body">
-        <!-- la tabla del menu-->
-        <div class="container">
-            <table class="table" align="center">
-                <tr>
-                    <th><a href=""> <center><img src="{{asset('images/fichas.jpg')}}" alt="fichas" class="img-thumbnail"/></center></a></th>
-                    <th><a href=""><center><img src="{{asset('images/inventario.jpg')}}" alt="inventario" class="img-thumbnail"/></center></a></th>
-                    <th><a href=""><center><img src="{{asset('images/solicitudes.jpg')}}" alt="solicitudes" class="img-thumbnail"/></center></a></th>
-                    <th><a href=""><center><img src="{{asset('images/procesos.jpg')}}" alt="procesos" class="img-thumbnail"/></center></a></th>
-                </tr>             
-                <tr align="center">
-                    <td>Fichas</td>
-                  <td>Inventario</td>
-                  <td>Solicitudes</td>
-                  <td>Procesos</td>
-                </tr>
-                <tr>
-                    <th><a href=""><center><img src="{{asset('images/reportes.jpg')}}" alt="reportes" class="img-thumbnail"/></center></a></th>
-                    <th><a href="{{route('administracion/buscar_usuario')}}"><center><img src="{{asset('images/admin.jpg')}}" alt="administracion" class="img-thumbnail"/></center></a></th>
-                    <th> <a href="#ingreso" data-toggle="modal">
-                         <center><img src="{{asset('images/ayuda.jpg')}}" alt="ayuda" class="img-thumbnail"/></center>
-                        </a>
-                    </th>
-                    <th><a href="{{route('usuario_app/salir')}}"><center><img src="{{asset('images/salir.jpg')}}" alt="salir" class="img-thumbnail"/></center></a></th>
-                </tr>  
-                 <tr align="center">
-                    <td>Reportes</td>
-                  <td>Administracion</td>
-                  <td>Ayuda</td>
-                  <td>Salir</td>
-                </tr>
+    <br><br>
+        <div class="container text-center">
+            <table class="table">             
+                <center>
+                    <img src="{{asset('images/esquema1.png')}}"/><br>
+                    <h4>Sistema Inform&aacute;tico que ayuda a controlar y agilizar de manera eficiente <br>los procesos de la gesti&oacute;n de activo fijo de la instituci&oacute;n</h4>
+                    <br><h3><img src="{{asset('images/bienvenida.png')}}"/>: {{ Auth::user()->nombre_usuario }}</h3>
+                </center>
             </table>
         </div>
-        <!--fin tabla de menu-->
-    </div>
+    <br><br><br><br>
+    
+
 </div>
-@stop   
+@stop
