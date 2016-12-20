e04a9d1f7c558b66056047e539b5d1f01c1f2155
Finalizando <krltrabajog@gmail.com>
2016-12-20 09:44:40 -0600

    Plantilla Buscar Ubicacion_org por cód de U/Dpto.

--- resources/views/catalogos/ubicacion_organizacional/buscar_ubicacion_org.blade.php
+++ resources/views/catalogos/ubicacion_organizacional/buscar_ubicacion_org.blade.php
@@ -1,9 +1,9 @@
-<!-- 
+{{-- 
      * Nombre del archivo: buscar_ubicacion_org.blade.php
      * Descripción: Pantalla para buscar ubicacion organizacional
      * Fecha de creación: 4/12/2016
      * Creado por: Karla Barrera
--->
+--}}
 @extends('plantillas.plantilla_base')
 @section('fecha_sistema')
 <p ALIGN=left>Fecha:<?=date('d/m/Y g:ia');?></p>
@@ -14,6 +14,7 @@
 @section('nombre_pantalla')
   <h4 class="text-center">Buscar ubicación organizacional</h4>
 @stop 
+<div>
 @section('menu_lateral')
 <div class="list-group">
     <a class="list-group-item active">Buscar ubicaci&oacute;n organizacional</a>
@@ -23,17 +24,17 @@
 @section('contenido')
 <div class="panel panel-default">
     <!-- filtro busqueda de ubicacion, los "&nbsp; son espacios"-->
-
-    
-                <div class="form-group" >
-                  <table class="table"> 
+<table class="table table-responsive">
+{!! Form::open(['route' => 'administracion.buscar_ubicacion.index', 'method' => 'GET', 'class' => 'navbar-form navbar-left pull-right', 'role' => 'search']) !!}
+  <div class="form-group" >
+      <table class="table table-responsive"> 
                     <tr border="0">
-                      <td>C&oacute;digo de Unidad/Departamento&nbsp;</td> 
-                      <td>  {!!Form::text('name',null,['class'=>'form-control', 'placeholder'=>'C&oacute;digo de Unidad/Departamento'])!!}</td> 
+                      <td>C&oacute;digo&nbsp;</td> 
+                      <td>  {!!Form::text('name',null,['class'=>'form-control', 'placeholder'=>'C&oacute;digo de Unidad/Depto'])!!}</td> 
                         &nbsp;<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Unidad/Departamento&nbsp;</td>
                       <td>
                         <select name="id_ubicacion_org" class="form-control" placeholder="Unidad o Departamento">
-                          <option disabled selected>Unidad o Departamento</option>
+                          <option disabled selected>Unidad/Depto</option>
                         </select>
                       </td> 
          <!--       </div>   -->
@@ -50,14 +51,13 @@
                   
 <!--   {!! Form::close() !!}  -->
     <!-- fin filtro busqueda ubicacion organizacional -->
-     
-
-
-    <table class="table">   
+<div class="form-group" >     
+<table class="table table-responsive">
+    <table class="table table-condensed">  
     <thead>
       <tr>
         <th>#&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
-        <th>C&oacute;digo de Unidad/Departamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+        <th>C&oacute;digo&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
         <th>Unidad/Departamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
         <th>Responsable&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
         <th>Alquilado&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
@@ -65,12 +65,31 @@
         <th>Seleccionar</th>        
       </tr>
     </thead>
-
-
-
-  </table>
+    <tbody> 
+             @foreach($ubicacion_org as $ubicacion)
+            <tr> 
+        
+                <td>{{$ubicacion->id_ubicacion_org}}</td>
+                <td>{{$ubicacion->codigo_unidad_dep}}</td>
+                <td>{{$ubicacion->nombre_unidad_dep}}</td>
+                <td>{{$ubicacion->nombre_responsable}}</td>
+                <td>&nbsp;&nbsp;{{$ubicacion->alquilado}}</td>
+                <td>&nbsp;&nbsp;{{$ubicacion->estado_registro}}</td>
+                <td><center>
+          {{--          <a href="{{route('administracion/editar_ubicacion',$ubicacion->id_ubicacion_org)}}" class="btn btn-danger"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> </a> 
+                    <a href="{{route('administracion/eliminar_ubicacion',$ubicacion->id_ubicacion_org)}}" onclick="return confirm('¿Seguro deseas eliminarlo?')" class="btn btn-warning"><span class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span></a>
+            --}}        </center>
+                </td>
+        
+                <!--<td><center>{!! Form::radio('seleccionar',$ubicacion->id_ubicacion_org, ['class' => 'form-control' , 'required' => 'required']) !!}</center></td>                      -->
+            </tr>
+           @endforeach
+      </tbody>
+    </table>
    
-<!--  {!! Form::close() !!}  -->
+{!! Form::close() !!} 
+</table>
+</div>
 
 <!-- Botones"--> 
     <div><br /><br />
@@ -88,5 +107,6 @@
     <!-- Fin botones-->
       </table> 
     </div>
+</div>
 </div>
 @stop   
