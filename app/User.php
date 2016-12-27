a1ec2535d8b4751191a1e6c17af965163b98b674
Juan Carlos Centeno <jcborellana@gmail.com>
2016-12-26 20:16:12 -0600

    Incorporado el borrado logico en el modelo user

--- app/User.php
+++ app/User.php
@@ -11,10 +11,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
     use EntrustUserTrait;
     use Authenticatable, CanResetPassword;
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
     protected $table = 'usuario_app';
     protected $fillable = [
         'email_usuario', 
