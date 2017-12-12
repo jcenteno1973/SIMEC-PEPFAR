48ea74e122ff624a30d7783a8fd36c723ab05886
Juan Carlos Centeno <jcborellana@gmail.com>
2017-12-12 12:27:16 -0600

    CRUD catalogo y desglose

--- resources/views/catalogos/editar_desglose.blade.php
+++ resources/views/catalogos/editar_desglose.blade.php
@@ -19,14 +19,14 @@
 @stop 
 @section('menu_lateral')
 <div class="list-group">
-    <a href="catalogos/nuevo_evento" class="list-group-item">Nuevo evento</a>
-    <a href="catalogos/buscar_evento" class="list-group-item">Buscar evento</a>
-    <a href="catalogos/nuevo_catalogo" class="list-group-item">Nuevo catalogo</a>
-    <a href="catalogos/buscar_catalogo" class="list-group-item">Buscar catalogo</a>
-    <a href="carga/nueva_carga" class="list-group-item">Nuevo componente</a>
-    <a href="carga/buscar_carga" class="list-group-item">Buscar componente</a>
-    <a href="carga/nueva_carga" class="list-group-item">Nuevo indicador</a>
-    <a href="carga/buscar_carga" class="list-group-item">Buscar indicador</a>
+    <a href="{{route('catalogos/nuevo_evento')}}" class="list-group-item">Nuevo evento</a>
+    <a href="{{route('catalogos/buscar_evento')}}" class="list-group-item">Buscar evento</a>
+    <a href="{{route('catalogos/nuevo_catalogo')}}" class="list-group-item">Nuevo catalogo</a>
+    <a href="{{route('catalogos/buscar_catalogo')}}" class="list-group-item">Buscar catalogo</a>
+    <a href="{{route('carga/nueva_carga')}}" class="list-group-item">Nuevo componente</a>
+    <a href="{{route('carga/buscar_carga')}}" class="list-group-item">Buscar componente</a>
+    <a href="{{route('carga/nueva_carga')}}" class="list-group-item">Nuevo indicador</a>
+    <a href="{{route('carga/buscar_carga')}}" class="list-group-item">Buscar indicador</a>
 </div>
 @stop
 @section('contenido')
