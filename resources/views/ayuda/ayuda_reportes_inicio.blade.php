8fedcb73b13bb287f7d36c8a3ba6bd97a0e0b6fe
Juan Carlos Centeno <jcborellana@gmail.com>
2018-01-14 15:10:07 -0600

    Ayuda reporte inicio

--- resources/views/ayuda/ayuda_reportes_inicio.blade.php
+++ resources/views/ayuda/ayuda_reportes_inicio.blade.php
@@ -1,8 +1,43 @@
-<?php
+<!--Boton de ayuda-->
+    <a href="#ingresosistema" class="btn btn- btn-primary" data-toggle="modal">Ayuda</a>
+        <div class="modal fade" id="ingresosistema" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog"> <!---div class="modal-dialog">-->
+                <div class="modal-content"> 
+                    <!--header de la ventana-->
+                    <div class="modal-header">                    
+                        <p class="modal-title">GENERAR REPORTES</p>
+                    </div>
+                    <!-- Contenido de la ventana -->
+                    <div class="modal-body">
+                    <table> 
+                        <tr>
+                            <td>
+                                <img src="{{asset('images/informativo.png')}}" alt="informativo"/>
+                            </td>
+                            <td>
+                                <p> Para generar un reporte siga los siguientes pasos:</p>
+                            </td>
+                        <tr>    
+                            <td>
+                                <p><font color="white">...</font></p>
+                            </td>
+                            <td>
+                                <ol>
+                                    <li><strong>Evento</strong>: Seleccione un evento de la lista desplegable</li>
+                                    <li><strong>Indicador</strong>: Seleccione un indicador de la lista desplegable</li>
+                                    <li><strong>Ver reporte</strong>: Visualiza el reporte seleccionado</li>
+                                </ol>
+                            </td>
+                        </tr>                                
+                    </table>  
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+                    </div>
+                    <!--footer de la ventana-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
 
+            </div>
+        </div>
+<!--FIN Boton  de ayuda-->
