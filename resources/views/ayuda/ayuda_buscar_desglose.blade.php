d40cf953db181ef291521a19fa8724e1dd87a3b0
Juan Carlos Centeno <jcborellana@gmail.com>
2018-01-21 01:09:22 -0600

    Ayuda buscar desglose

---
+++ resources/views/ayuda/ayuda_buscar_desglose.blade.php
@@ -0,0 +1,33 @@
+<!--Boton de ayuda-->
+    <a href="#ayuda" class="btn btn- btn-primary" data-toggle="modal">Ayuda</a>
+        <div class="modal fade" id="ayuda" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog"> <!---div class="modal-dialog">-->
+                <div class="modal-content"> 
+                    <!--header de la ventana-->
+                    <div class="modal-header">                    
+                        <p class="modal-title">BUSCAR DESGLOSE</p>
+                    </div>
+                    <!-- Contenido de la ventana -->
+                    <div class="modal-body">
+                     <table class="table table-striped table-bordered">
+                            
+                            <tr>
+                              <td><a class="btn btn-warning"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>  
+                              <td>Edita el desglose</td>
+                            </tr>
+                           
+                            <tr>
+                               <td><a  class="btn btn-danger"><span class="glyphicon glyphicon-erase" aria-hidden="true"></span></a></td> 
+                               <td>Borra el desglose</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <!--footer de la ventana-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+<!--FIN Boton de  ayuda-->
