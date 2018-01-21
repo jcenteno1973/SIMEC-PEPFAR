ba55af07948d580110b887c64c9045929bf4c944
Juan Carlos Centeno <jcborellana@gmail.com>
2018-01-21 00:29:29 -0600

    Ayuda editar catalogo

---
+++ resources/views/ayuda/ayuda_editar_catalogo.blade.php
@@ -0,0 +1,49 @@
+<!--Boton de ayuda-->
+    <a href="#ayuda" class="btn btn- btn-primary" data-toggle="modal">Ayuda</a>
+        <div class="modal fade" id="ayuda" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog"> <!---div class="modal-dialog">-->
+                <div class="modal-content"> 
+                    <!--header de la ventana-->
+                    <div class="modal-header">                    
+                        <p class="modal-title">EDITAR CATÁLOGO</p>
+                    </div>
+                    <!-- Contenido de la ventana -->
+                    <div class="modal-body">
+                    <table> 
+                        <tr>
+                            <td>
+                                <img src="{{asset('images/informativo.png')}}" alt="informativo"/>
+                            </td>
+                            <td>
+                                <p> Para editar un catálogo:</p>
+                            </td>
+                        <tr>    
+                            <td>
+                                <p><font color="white">...</font></p>
+                            </td>
+                            <td>
+                                <ul>
+                                    
+                                    <li><strong>Nombre de catálogo</strong>: Digitar el nombre del catálogo con un maximo de 35 caracteres.</li>
+                                    
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p ><font color="white">...</font></p>
+                            </td>
+                        </tr>                    
+                    </table>  
+
+                    </div>
+                    <!--footer de la ventana-->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+<!--FIN Boton  de ayuda-->
+        
--- resources/views/catalogos/editar_catalogo.blade.php
+++ resources/views/catalogos/editar_catalogo.blade.php
@@ -53,7 +53,7 @@
           <td>
              <button type="submit" class="btn btn-primary">Guardar</button>  
               <a href="{{route('catalogos/buscar_catalogo')}}" class="btn btn-primary"> Regresar</a>
-              @include('ayuda/ayuda_nuevo_usuario') 
+              @include('ayuda/ayuda_editar_catalogo') 
           </td>
         </tr>        
         </table>
